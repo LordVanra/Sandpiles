f3683fc5b1827bf6e3f18d9c1e3888258e90fb81
LordVanra <146479458+LordVanra@users.noreply.github.com>
2026-02-07 20:23:40 -0500

    Return to 3D

--- graph.py
+++ graph.py
@@ -1,6 +1,5 @@
 import networkx as nx
 
-def create_random_2_regular(n):
-    # Creating a 2D grid graph
-    G = nx.grid_graph(dim=[n, n])
+def create_random_3_regular(n):
+    G = nx.grid_graph(dim=[n, n, n])
     return nx.convert_node_labels_to_integers(G)
